d7e0d2f417a1fdfa1ffbdfcdcffbfa12141b2b21
Sunny <ring2rings93@gmail.com>
2019-03-28 23:20:07 +0100

    bubble sort by

--- main.rb
+++ main.rb
@@ -7,60 +7,22 @@ def bubble_sort(array)
   end
   array
 end
-
-def bubble_sort_by(array)
-  reps = array.length - 1
-  reps.times do |i|
-    (reps - i).times do |j|
-      condition =
-      array[j], array[j+1] = array[j+1], array[j] if condition
-    end
-  end
-  array
-end
-
 arr = [5, 2, 8, 1]
-
 p bubble_sort arr
 
-
-
-def bubble_sort(array)
-  reps = array.length - 1
-  reps.times do |i|
-    (reps - i).times do |j|
-      array[j], array[j+1] = array[j+1], array[j] if array[j] > array[j+1]
-    end
-  end
-  array
-end
-
-
-
-
-
-
-
 def bubble_sort_by(arr)
-
     arrary_length = arr.length - 1
     arrary_length.times do |x|
         (arrary_length - x).times do |i|
-            if block_given?
-                result  = yield(arr[i], arr[i+1]) 
-            end
-            if result > 0
-            arr[i],arr[i+1] = arr[i+1], arr[i]
-            end
+            result = 0
+            result  = yield(arr[i], arr[i+1]) if block_given?
+            arr[i],arr[i+1] = arr[i+1], arr[i] if result > 0
         end                 
     end
-    # yield.call
     arr
 end
-
 nums = [3,1,5,12,7]
 p bubble_sort_by(nums) { |n1, n2| n1 - n2  }
-
 
 
 
