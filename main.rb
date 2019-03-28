462f487cd365897010b4de3911df9f002c3bb1a9
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-03-28 13:51:22 -0500

    [add] bubble _sort and WIP bobble_sorted_by

---
+++ main.rb
@@ -0,0 +1,24 @@
+def bubble_sort(array)
+  reps = array.length - 1
+  reps.times do |i|
+    (reps - i).times do |j|
+      array[j], array[j+1] = array[j+1], array[j] if array[j] > array[j+1]
+    end
+  end
+  array
+end
+
+def bouble_sort_by(array)
+  reps = array.length - 1
+  reps.times do |i|
+    (reps - i).times do |j|
+      condition =
+      array[j], array[j+1] = array[j+1], array[j] if condition
+    end
+  end
+  array
+end
+
+arr = [5, 2, 8, 1]
+
+p bubble_sort arr
