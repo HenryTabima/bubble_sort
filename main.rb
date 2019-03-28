dc9b26536b3a1704cd01b23a80910afbb519a313
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-03-28 17:51:03 -0500

    [fixes] linting errors

--- main.rb
+++ main.rb
@@ -2,42 +2,25 @@ def bubble_sort(array)
   reps = array.length - 1
   reps.times do |i|
     (reps - i).times do |j|
-      array[j], array[j+1] = array[j+1], array[j] if array[j] > array[j+1]
+      array[j], array[j + 1] = array[j + 1], array[j] if array[j] > array[j + 1]
     end
   end
   array
 end
+
 arr = [5, 2, 8, 1]
 p bubble_sort arr
 
 def bubble_sort_by(arr)
-    arrary_length = arr.length - 1
-    arrary_length.times do |x|
-        (arrary_length - x).times do |i|
-            result = 0
-            result  = yield(arr[i], arr[i+1]) if block_given?
-            arr[i],arr[i+1] = arr[i+1], arr[i] if result > 0
-        end                 
+  arrary_length = arr.length - 1
+  arrary_length.times do |x|
+    (arrary_length - x).times do |i|
+      result = yield(arr[i], arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if result > 0
     end
-    arr
+  end
+  arr
 end
-nums = [3,1,5,12,7]
-p bubble_sort_by(nums) { |n1, n2| n1 - n2  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+nums = [3, 1, 5, 12, 7]
+p bubble_sort_by(nums) { |n1, n2| n1 - n2 }
